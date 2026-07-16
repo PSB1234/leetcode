245f3576e27aa45185a63a545435177aa88dfd9d
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:19:12 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.ts
@@ -0,0 +1,13 @@
+// Last updated: 16/7/2026, 9:19:13 pm
+function lengthOfLIS(nums: number[]): number {
+    const dp: number[] = new Array(nums.length).fill(1);
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                dp[i] = Math.max(dp[i], 1 + dp[j]);
+            }
+        }
+    }
+    return Math.max(...dp);
+};
