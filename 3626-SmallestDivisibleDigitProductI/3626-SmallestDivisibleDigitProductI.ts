f177228b11298083b0d4fc84bd7a3705d081485a
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:15:52 +0530

    Create 3626-SmallestDivisibleDigitProductI

---
+++ 3626-SmallestDivisibleDigitProductI/3626-SmallestDivisibleDigitProductI.ts
@@ -0,0 +1,19 @@
+// Last updated: 16/7/2026, 9:15:53 pm
+function multiplyDigits(n: number): number {
+    let ans: number = 1;
+    while (n > 0) {
+        ans *= n % 10;
+        n = Math.floor(n / 10);
+    }
+    return ans;
+}
+function smallestNumber(n: number, t: number): number {
+    while (n <= 100) {
+        if (multiplyDigits(n) % t === 0) {
+            return n;
+        } else {
+            n++;
+        }
+    }
+    return n;
+};
