d950c8a58f40375a15c952a4c19f2bb6d1ad0534
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:35 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.ts
@@ -0,0 +1,21 @@
+// Last updated: 16/7/2026, 9:21:36 pm
+function combinationSum2(candidates: number[], target: number): number[][] {
+    const ans:number[][] = []
+    function recursion(candidates: number[],start:number,subset:number[], value: number){
+        if(value<0){
+            return;
+        }
+        if(value === 0){
+            ans.push([...subset]);
+            return;
+        }
+        for(let i = start;i<candidates.length;i++){
+            if (i > start && candidates[i] === candidates[i - 1]) continue;
+            subset.push(candidates[i]);
+            recursion(candidates,i+1,subset,value-candidates[i]);
+            subset.pop();
+        }
+    }
+    recursion(candidates.sort(),0,[],target);
+    return ans;
+};
