550e7d790f80f84541708bde42bd57ebe320892f
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:20 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.ts
@@ -0,0 +1,18 @@
+// Last updated: 16/7/2026, 9:18:20 pm
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+        if (nums[middle] === target) {
+            return middle;
+        }
+        if (nums[middle] > target) {
+            right = middle - 1;
+        }
+        if (nums[middle] < target) {
+            left = middle + 1;
+        }
+    }
+    return -1;
+};
