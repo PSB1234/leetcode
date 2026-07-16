7bb4184b4af381a4d103233580abcd25e83ff347
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:20:04 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.ts
@@ -0,0 +1,13 @@
+// Last updated: 16/7/2026, 9:20:05 pm
+function rob(nums: number[]): number {
+    const ans: number[] = [];
+    ans.push(0);
+    ans.push(nums[0]);
+    for (let i = 2; i <= nums.length; i++) {
+        const a= ans[i - 1];
+        const b= ans[i - 2] + nums[i-1];
+        ans.push(Math.max(a,b));
+    }
+    return ans[nums.length];
+};
+rob([1,2,3,1])
