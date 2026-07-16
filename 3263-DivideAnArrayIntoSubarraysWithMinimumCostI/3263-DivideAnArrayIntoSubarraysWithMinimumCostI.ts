aa6ea39935e7d53aa87aa022258af0ae778f8bd4
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:05 +0530

    Create 3263-DivideAnArrayIntoSubarraysWithMinimumCostI

---
+++ 3263-DivideAnArrayIntoSubarraysWithMinimumCostI/3263-DivideAnArrayIntoSubarraysWithMinimumCostI.ts
@@ -0,0 +1,7 @@
+// Last updated: 16/7/2026, 9:16:06 pm
+function minimumCost(nums: number[]): number {
+    const first = nums.shift();
+    nums.sort((a, b) => a - b);
+    return first! + nums[0] + nums[1];
+
+};
