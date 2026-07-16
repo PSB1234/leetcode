85c6436467ade53d999a13e5349ee47ae473c242
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:17:41 +0530

    Create 1544-CountGoodNodesInBinaryTree

---
+++ 1544-CountGoodNodesInBinaryTree/1544-CountGoodNodesInBinaryTree.ts
@@ -0,0 +1,29 @@
+// Last updated: 16/7/2026, 9:17:42 pm
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function goodNodes(root: TreeNode | null): number {
+    let count = 0;
+    function dfs(root: TreeNode | null, prev: number) {
+        if (root === null) return;
+        if (root.val >= prev) {
+            prev = root.val;
+            count++;
+        }
+        dfs(root.left, prev);
+        dfs(root.right, prev);
+    }
+    dfs(root, -(10 ** 4));
+    return count;
+};
