f27e824a357800c1bba4dcce852cf1fae25cd44e
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:44 +0530

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.ts
@@ -0,0 +1,13 @@
+// Last updated: 16/7/2026, 9:16:45 pm
+function countOperations(num1: number, num2: number): number {
+    let step = 0;
+    while (num1 !== 0 && num2 !== 0) {
+        if (num1 >= num2) {
+            num1 -= num2;
+        } else {
+            num2 -= num1;
+        }
+        step++;
+    }
+    return step;
+};
