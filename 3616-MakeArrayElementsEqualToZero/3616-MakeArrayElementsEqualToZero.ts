d14f9c42c1f6ba5f656f9bae52a5876f9a2fee0b
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:15:53 +0530

    Create 3616-MakeArrayElementsEqualToZero

---
+++ 3616-MakeArrayElementsEqualToZero/3616-MakeArrayElementsEqualToZero.ts
@@ -0,0 +1,24 @@
+// Last updated: 16/7/2026, 9:15:54 pm
+function simulation(nums: number[], index: number): number {
+    let leftSum = 0;
+    let rightSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i < index) {
+            leftSum += nums[i];
+        } else {
+            rightSum += nums[i];
+        }
+    }
+    if (Math.abs(rightSum - leftSum) === 0) return 2;
+    if (Math.abs(rightSum - leftSum) === 1) return 1;
+    return 0;
+}
+function countValidSelections(nums: number[]): number {
+    let selection = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            selection += simulation(nums, i);
+        }
+    }
+    return selection;
+};
