b2e923dc729ea426edc5eac75ae74b5d90cedc19
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:06 +0530

    Create 1018-LargestPerimeterTriangle

---
+++ 1018-LargestPerimeterTriangle/1018-LargestPerimeterTriangle.ts
@@ -0,0 +1,12 @@
+// Last updated: 16/7/2026, 9:18:07 pm
+function largestPerimeter(nums: number[]): number {
+    nums.sort((a, b) => b - a);
+    let perimeter = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] < nums[i + 1] + nums[i + 2]) {
+            perimeter = Math.max(perimeter, nums[i] + nums[i + 1] + nums[i + 2]);
+        }
+    }
+    return perimeter;
+};
+
