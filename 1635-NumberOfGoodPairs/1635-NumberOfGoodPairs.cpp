c4e78b0ca06338ec7503738caeeb3fafbc167c42
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:17:32 +0530

    Create 1635-NumberOfGoodPairs

---
+++ 1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.cpp
@@ -0,0 +1,15 @@
+// Last updated: 16/7/2026, 9:17:33 pm
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count = 0;
+        for(int i =0 ; i<nums.size();i++){
+            for(int j =0 ; j<nums.size();j++){
+              if((nums[i]==nums[j])&&i<j){
+                count++;
+              }
+          }
+        }
+        return count;
+    }
+};
