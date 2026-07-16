dfb0d340eba63d80fe5f4dbb800737b8403c1426
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:17 +0530

    Create 883-CarFleet

---
+++ 883-CarFleet/883-CarFleet.ts
@@ -0,0 +1,24 @@
+// Last updated: 16/7/2026, 9:18:18 pm
+function carFleet(target: number, position: number[], speed: number[]): number {
+    const car: [position: number, speed: number, time: number][] = [];
+    for (let i = 0; i < position.length; i++) {
+        car[i] = [position[i], speed[i], (target - position[i]) / speed[i]];
+    }
+    car.sort((a, b) => b[0] - a[0]);
+    let slowestCar = 0;
+    let j = 1;
+    let Fleet = 0;
+    while (slowestCar < car.length && j < car.length) {
+        let timeOfSlowestCar = car[slowestCar][2];
+        let timeOfAnotherCar = car[j][2];
+        if (timeOfSlowestCar >= timeOfAnotherCar) {
+            j++;
+        } else {
+            Fleet++;
+            slowestCar = j;
+            j++;
+        }
+    }
+    return Fleet + 1;
+
+};
