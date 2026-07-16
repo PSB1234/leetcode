55c65654296f9478f37a01905c7104b2feceb125
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:12 +0530

    Create 2859-AddTwoPromises

---
+++ 2859-AddTwoPromises/2859-AddTwoPromises.js
@@ -0,0 +1,14 @@
+// Last updated: 16/7/2026, 9:16:13 pm
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+return await promise1 + await promise2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
