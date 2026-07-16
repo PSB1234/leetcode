c081a79ec100d58e72b34bb9aafa4d07b5897bd7
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:22:10 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.ts
@@ -0,0 +1,17 @@
+// Last updated: 16/7/2026, 9:22:10 pm
+function lengthOfLongestSubstring(s: string): number {
+    const set = new Set<string>();
+    let left = 0;
+    let right = 0;
+    let max = 0;
+    while (right < s.length) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++
+        }
+        set.add(s[right]);
+        max = Math.max(max, right - left + 1);
+        right++;
+    }
+    return max;
+};
