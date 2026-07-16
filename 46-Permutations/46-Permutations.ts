da53a32c7807d33b9353b9d5d2a2b6e72ebc4f5c
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:32 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.ts
@@ -0,0 +1,18 @@
+// Last updated: 16/7/2026, 9:21:33 pm
+function permute(nums: number[]): number[][] {
+    const ans:number[][] = [];
+    const sol:number[] = [];
+    function recursion(arr:number[]){
+        if(arr.length === 0){
+            ans.push([...sol]);
+            return;
+        }
+        for(let i=0;i<arr.length;i++){
+            sol.push(arr[i]);
+            recursion(arr.filter((_,index)=>index!==i));
+            sol.pop();
+        }
+    }
+    recursion(nums);
+    return ans;
+};
