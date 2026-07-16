240eb1b43c760dc60d3c85a165d4c9d8647b47c6
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:17:31 +0530

    Create 1700-MinimumTimeToMakeRopeColorful

---
+++ 1700-MinimumTimeToMakeRopeColorful/1700-MinimumTimeToMakeRopeColorful.ts
@@ -0,0 +1,18 @@
+// Last updated: 16/7/2026, 9:17:32 pm
+function minCost(colors: string, neededTime: number[]): number {
+    let totalTime = 0;
+    for (let i = 0; i < colors.length; i++) {
+        if (colors[i] == colors[i + 1]) {
+            if (neededTime[i] < neededTime[i + 1]) {
+                totalTime += neededTime[i];
+
+            } else {
+                totalTime += neededTime[i + 1];
+                let temp = neededTime[i + 1];
+                neededTime[i + 1] = neededTime[i];
+                neededTime[i] = temp;
+            }
+        }
+    }
+    return totalTime;
+};
