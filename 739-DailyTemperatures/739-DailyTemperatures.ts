98fef1101a185fbb40b641f05e521ebee16933a0
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:26 +0530

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.ts
@@ -0,0 +1,15 @@
+// Last updated: 16/7/2026, 9:18:27 pm
+function dailyTemperatures(temperatures: number[]): number[] {
+    const result: number[] = new Array(temperatures.length).fill(0);
+    const stack: number[] = [];
+
+    for (let i = 0; i < temperatures.length; i++) {
+        while (stack.length > 0 && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+            const index = stack.pop()!;
+            result[index] = i - index;
+        }
+        stack.push(i);
+    }
+
+    return result;
+};
