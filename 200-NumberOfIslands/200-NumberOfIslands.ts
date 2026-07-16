ce4f7097ec42be17b7da994d31e413f0309ed093
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:19:59 +0530

    Create 200-NumberOfIslands

---
+++ 200-NumberOfIslands/200-NumberOfIslands.ts
@@ -0,0 +1,28 @@
+// Last updated: 16/7/2026, 9:20:00 pm
+function numIslands(grid: string[][]): number {
+    let islands = 0;
+    function dfs(i: number, j: number) {
+        if (
+            i < 0 ||
+            i >= grid.length ||
+            j < 0 ||
+            j >= grid[0].length ||
+            grid[i][j] === '0'
+        ) return;
+
+        grid[i][j] = '0';
+        dfs(i + 1, j);
+        dfs(i, j + 1);
+        dfs(i - 1, j);
+        dfs(i, j - 1);
+    }
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === "1") {
+                dfs(i, j);
+                islands++;
+            }
+        }
+    }
+    return islands;
+};
