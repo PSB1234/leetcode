31bcba6bdc06257732863d137472487df5f65950
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:07 +0530

    Create 1014-KClosestPointsToOrigin

---
+++ 1014-KClosestPointsToOrigin/1014-KClosestPointsToOrigin.ts
@@ -0,0 +1,8 @@
+// Last updated: 16/7/2026, 9:18:08 pm
+function kClosest(points: number[][], k: number): number[][] {
+    return points
+        .map(p => [p, p[0] * p[0] + p[1] * p[1]] as [number[], number])
+        .sort((a, b) => a[1] - b[1])
+        .slice(0, k)
+        .map(p => p[0]);
+};
