bdd4e45eae962772908ea9d2c088fbf7927c8b58
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:45 +0530

    Create 25-ReverseNodesInKGroup

---
+++ 25-ReverseNodesInKGroup/25-ReverseNodesInKGroup.ts
@@ -0,0 +1,44 @@
+// Last updated: 16/7/2026, 9:21:46 pm
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getKthNode(groupPrev: ListNode | null, k: number) {
+    while (groupPrev != null && k > 0) {
+        groupPrev = groupPrev.next;
+        k--;
+    }
+    return groupPrev;
+}
+function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
+    const dummyNode = new ListNode(0, head);
+    let groupPrev: ListNode | null = dummyNode;
+    while (true) {
+        const kthNode = getKthNode(groupPrev, k);
+        if (kthNode != null) {
+            const groupNext = kthNode.next;
+            let prev = kthNode.next;
+            let curr: ListNode | null = groupPrev!.next;
+            while (curr !== groupNext) {
+                const tmp = curr?.next!;
+                curr!.next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+            const tmp: ListNode | null = groupPrev!.next;
+            groupPrev!.next = kthNode;
+            groupPrev = tmp;
+        } else {
+            break;
+        }
+    }
+    return dummyNode.next;
+};
