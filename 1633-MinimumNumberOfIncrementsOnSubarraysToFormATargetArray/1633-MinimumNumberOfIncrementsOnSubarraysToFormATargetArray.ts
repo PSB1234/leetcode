e314ef996c6323fc4c06c22beaa20f03d965fc19
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:17:34 +0530

    Create 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray

---
+++ 1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/1633-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.ts
@@ -0,0 +1,10 @@
+// Last updated: 16/7/2026, 9:17:35 pm
+function minNumberOperations(target: number[]): number {
+    let result = target[0];
+    for (let i = 1; i < target.length; i++) {
+        if (target[i - 1] < target[i]) {
+            result += target[i] - target[i - 1];
+        }
+    }
+    return result;
+};
