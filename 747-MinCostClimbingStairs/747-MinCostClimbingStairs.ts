eeb6e5a02c8e142d6c5c4a9a5300d2ce21c9980c
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:22 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.ts
@@ -0,0 +1,7 @@
+// Last updated: 16/7/2026, 9:18:23 pm
+function minCostClimbingStairs(cost: number[]): number {
+    for(let i = 1;i< cost.length;i++){
+        cost[i] += Math.min(cost[i-1],cost[i-2]!==undefined?cost[i-2]:0);
+    }
+    return Math.min(cost[cost.length-1],cost[cost.length-2]);
+};
