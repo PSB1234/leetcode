b6d6701aca6b8a4f947b2db988192340220650d2
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:39 +0530

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.ts
@@ -0,0 +1,26 @@
+// Last updated: 16/7/2026, 9:18:40 pm
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function isSameTree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if (!root && !subRoot) return true;
+    if (!root || !subRoot || root.val !== subRoot.val) return false;
+    return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+}
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if (!subRoot) return true;
+    if (!root) return false;
+    if (isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+};
