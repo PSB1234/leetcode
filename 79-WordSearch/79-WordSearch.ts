2da33b789ba18eee1f586dad66f8594c565804ed
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:08 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/79-WordSearch.ts
@@ -0,0 +1,24 @@
+// Last updated: 16/7/2026, 9:21:09 pm
+
+function exist(board: string[][], word: string): boolean {
+    const rows = board.length;
+    const cols = board[0].length;
+    function checkSides(i:number,j:number,k:number):boolean{
+        if (k === word.length) return true;
+        if(i<0||j<0||i>=rows||j>=cols || board[i][j]!== word[k]) return false;
+        const temp = board[i][j];
+        board[i][j] = '#';
+        const found = checkSides(i+1,j,k+1)||
+        checkSides(i-1,j,k+1)||
+        checkSides(i,j+1,k+1)||
+        checkSides(i,j-1,k+1)
+        board[i][j] = temp;
+        return found;
+    }
+    for(let i = 0;i<board.length;i++){
+        for(let j = 0;j<board[0].length;j++){
+            if(checkSides(i,j,0)) return true;
+        }
+    }
+    return false;
+};
