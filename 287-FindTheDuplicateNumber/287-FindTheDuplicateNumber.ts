2bb874340b3e28f8f09d37985638bde94cb32cd7
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:19:16 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.ts
@@ -0,0 +1,8 @@
+// Last updated: 16/7/2026, 9:19:17 pm
+function findDuplicate(nums: number[]): number {
+    let i = 0;
+    while (nums[i]) {
+        [nums[i], i] = [0, nums[i]];
+    }
+    return i;
+};
