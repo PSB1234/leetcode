e00cadc073b636369f61d8b480cfd23c3472994e
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:55 +0530

    Create 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores

---
+++ 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores/2112-MinimumDifferenceBetweenHighestAndLowestOfKScores.ts
@@ -0,0 +1,10 @@
+// Last updated: 16/7/2026, 9:16:56 pm
+function minimumDifference(nums: number[], k: number): number {
+    if (k === 1) return 0;
+    nums.sort((a, b) => a - b);
+    let min = Infinity;
+    for (let i = 0; i <= nums.length - k; i++) {
+        min = Math.min(min, nums[i + k - 1] - nums[i]);
+    }
+    return min;
+};
