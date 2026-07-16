a0a15e5e2ff4afb4fdf1ef1d2dc26dc5a007d9fe
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:24 +0530

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.ts
@@ -0,0 +1,15 @@
+// Last updated: 16/7/2026, 9:18:25 pm
+function nextGreatestLetter(letters: string[], target: string): string {
+    let left = 0;
+    let right = letters.length - 1;
+    let middle = Math.floor((left + right) / 2);
+    while (left < right) {
+        middle = Math.floor((left + right) / 2);
+        if (target.charCodeAt(0) < letters[middle].charCodeAt(0)) {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return (target.charCodeAt(0) < letters[right].charCodeAt(0)) ? letters[right] : letters[0];
+};
