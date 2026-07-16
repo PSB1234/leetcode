9fe4702f88f31ae0e431045f5b2e7b446e2725eb
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:17:58 +0530

    Create 1153-ProductSalesAnalysisI

---
+++ 1153-ProductSalesAnalysisI/1153-ProductSalesAnalysisI.sql
@@ -0,0 +1,3 @@
+-- Last updated: 16/7/2026, 9:17:59 pm
+# Write your MySQL query statement below
+select Product.product_name,Sales.year,Sales.price from Sales inner join Product on Sales.product_id = Product.product_id;
