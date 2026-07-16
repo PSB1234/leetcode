bc41b62446a23a0ffddb3a56ac8a9f71174bdaf5
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:55 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ 17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.ts
@@ -0,0 +1,30 @@
+// Last updated: 16/7/2026, 9:21:56 pm
+function letterCombinations(digits: string): string[] {
+    const alphaMap: Record<string, string[]> = {
+        "2": ['a', 'b', 'c'],
+        "3": ['d', 'e', 'f'],
+        "4": ['g', 'h', 'i'],
+        "5": ['j', 'k', 'l'],
+        "6": ['m', 'n', 'o'],
+        "7": ['p', 'q', 'r', 's'],
+        "8": ['t', 'u', 'v'],
+        "9": ['w', 'x', 'y', 'z'],
+      };
+      
+    const ans:string[] = [];
+    let temp = '';
+    function recursion(digit:number){
+        if(digit>=digits.length){
+            ans.push(temp);
+            return;
+        }
+        const numberpad = alphaMap[digits[digit]];
+        for(let i = 0;i<numberpad.length;i++){
+            temp += numberpad[i];
+            recursion(digit+1);
+            temp = temp.slice(0,-1);
+        }
+    }
+    recursion(0);
+    return ans;
+};
