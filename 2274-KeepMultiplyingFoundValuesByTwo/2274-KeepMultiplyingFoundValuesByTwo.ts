8c8a3c9289893af3c13f4255b2fd0d6297b55d74
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:45 +0530

    Create 2274-KeepMultiplyingFoundValuesByTwo

---
+++ 2274-KeepMultiplyingFoundValuesByTwo/2274-KeepMultiplyingFoundValuesByTwo.ts
@@ -0,0 +1,12 @@
+// Last updated: 16/7/2026, 9:16:46 pm
+function findFinalValue(nums: number[], original: number): number {
+    const numSet = new Set(nums);
+    while (true) {
+        if (numSet.has(original)) {
+            original *= 2;
+        } else {
+            break;
+        }
+    }
+    return original;
+};
