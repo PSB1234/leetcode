6711252c5db2e47507ba3a97077bdb7c7b3ae75a
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:19:25 +0530

    Create 239-SlidingWindowMaximum

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.ts
@@ -0,0 +1,22 @@
+// Last updated: 16/7/2026, 9:19:25 pm
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    const queue: number[] = [];
+    const ans: number[] = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        while (queue.length > 0 && nums[queue[queue.length - 1]] <= nums[i]) {
+            queue.pop();
+        }
+        queue.push(i);
+
+        if (queue[0] <= i - k) {
+            queue.shift();
+        }
+
+        if (i >= k - 1) {
+            ans.push(nums[queue[0]]);
+        }
+    }
+
+    return ans;
+};
