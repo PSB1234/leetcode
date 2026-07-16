c6b908cfa6bd46449d9f55584275eec8a0cc774e
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:03 +0530

    Create 1023-TimeBasedKeyValueStore

---
+++ 1023-TimeBasedKeyValueStore/1023-TimeBasedKeyValueStore.ts
@@ -0,0 +1,47 @@
+// Last updated: 16/7/2026, 9:18:04 pm
+
+class TimeMap {
+    private map = new Map<string, [timeStamp: number, value: string][]>();
+
+    constructor() { }
+
+    set(key: string, value: string, timestamp: number): void {
+        const existing = this.map.get(key);
+        if (existing) {
+            existing.push([timestamp, value]);
+        } else {
+            this.map.set(key, [[timestamp, value]]);
+        }
+    }
+    get(key: string, timestamp: number): string {
+        const existing = this.map.get(key);
+        if (existing) {
+            return this.binarySearch(existing, timestamp);
+        } else {
+            return ""
+        }
+    }
+    private binarySearch(arr: [timeStamp: number, value: string][], target: number): string {
+        let left = 0;
+        let right = arr.length - 1;
+        let res = "";
+        while (left <= right) {
+            const mid = left + Math.floor((right - left) / 2);
+            if (arr[mid][0] > target) {
+                right = mid - 1;
+            }
+            else {
+                res = arr[mid][1];
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
