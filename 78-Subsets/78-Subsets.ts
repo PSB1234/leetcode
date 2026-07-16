37d6e94b7b254f00f2cdcc1eca2b1daa504f5202
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:09 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.ts
@@ -0,0 +1,17 @@
+// Last updated: 16/7/2026, 9:21:10 pm
+function subsets(nums: number[]): number[][] {
+    const array: number[][] = [];
+    const subset: number[] = [];
+    function dfs(index: number): void {
+        if (index >= nums.length) {
+            array.push([...subset]);
+            return;
+        }
+        subset.push(nums[index]);
+        dfs(index + 1);
+        subset.pop();
+        dfs(index + 1);
+    }
+    dfs(0);
+    return array;
+};
