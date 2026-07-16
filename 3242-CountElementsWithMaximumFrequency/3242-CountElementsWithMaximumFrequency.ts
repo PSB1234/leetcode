8e24c03431f8c95336f42543f88b232d6d8a7d79
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:08 +0530

    Create 3242-CountElementsWithMaximumFrequency

---
+++ 3242-CountElementsWithMaximumFrequency/3242-CountElementsWithMaximumFrequency.ts
@@ -0,0 +1,17 @@
+// Last updated: 16/7/2026, 9:16:09 pm
+function maxFrequencyElements(nums: number[]): number {
+    let map = new Map<number, number>();
+    let maxFreq = 0;
+    for (let num of nums) {
+        let freq = (map.get(num) || 0) + 1;
+        map.set(num, freq);
+        maxFreq = Math.max(maxFreq, freq);
+    }
+    let count = 0;
+    for (let freq of map.values()) {
+        if (freq === maxFreq) {
+            count++;
+        }
+    }
+    return count*maxFreq;
+};
