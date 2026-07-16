c0517ce2fa269f0fb55242cf543900a0876f5e57
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:21:25 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.ts
@@ -0,0 +1,16 @@
+// Last updated: 16/7/2026, 9:21:26 pm
+function canJump(nums: number[]): boolean {
+    const ans: boolean[] = new Array<boolean>(nums.length).fill(false);
+    ans[nums.length - 1] = true;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        const start = i + 1;
+        const jumpLimit = i + nums[i];
+        const jumpRange = Math.min(nums.length - 1, jumpLimit);
+        for (let j = start; j <= jumpRange; j++) {
+            if (ans[j] === true) {
+                ans[i] = true;
+            }
+        }
+    }
+    return ans[0];
+};
