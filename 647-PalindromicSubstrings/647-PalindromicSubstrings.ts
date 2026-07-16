196c009b5645f7b571ca641584db551d39861458
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:34 +0530

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.ts
@@ -0,0 +1,22 @@
+// Last updated: 16/7/2026, 9:18:35 pm
+function countSubstrings(s: string): number {
+    let res = 0;
+    for(let i= 0;i<s.length;i++){
+        let l = i;
+        let r = i;
+        while(l>=0 && r<s.length && s[l]===s[r]){
+            res++;
+            l--;
+            r++;
+        }
+         l = i;
+         r = i+1;
+        while(l>=0 && r<s.length && s[l]===s[r]){
+            res++;
+            l--;
+            r++;
+        }
+        
+    }
+    return res;
+};
