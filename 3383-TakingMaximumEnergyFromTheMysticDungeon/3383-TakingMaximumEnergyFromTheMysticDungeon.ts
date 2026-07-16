c71f349745fc34581f51e6c3f8d1f19a3171d17a
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:16:01 +0530

    Create 3383-TakingMaximumEnergyFromTheMysticDungeon

---
+++ 3383-TakingMaximumEnergyFromTheMysticDungeon/3383-TakingMaximumEnergyFromTheMysticDungeon.ts
@@ -0,0 +1,17 @@
+// Last updated: 16/7/2026, 9:16:02 pm
+function maximumEnergy(energy: number[], k: number): number {
+    const n = energy.length;
+    const dp = Array(n).fill(0);
+    let ans = -Infinity;
+
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i] = energy[i];
+        if (i + k < n) dp[i] += dp[i + k];
+        ans = Math.max(ans, dp[i]);
+    }
+
+    return ans;
+
+
+}
+
