dc3932c6d35de4198a5864c9199fd3f01566021c
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:20:56 +0530

    Create 91-DecodeWays

---
+++ 91-DecodeWays/91-DecodeWays.ts
@@ -0,0 +1,15 @@
+// Last updated: 16/7/2026, 9:20:57 pm
+function numDecodings(s: string): number {
+    const dp: Array<number> = Array.from({ length: s.length + 1 }, () => 1);
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === '0') {
+            dp[i] = 0;
+        } else {
+            dp[i] = dp[i + 1];
+        }
+        if (i + 1 < s.length && (s[i] === '1' || (s[i] === '2' && ('0123456'.includes(s[i + 1]))))) {
+            dp[i] += dp[i + 2];
+        }
+    }
+    return dp[0];
+};
