21581d069032e1cca27320218630b054faa1b306
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:47 +0530

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.ts
@@ -0,0 +1,19 @@
+// Last updated: 16/7/2026, 9:18:48 pm
+function characterReplacement(s: string, k: number): number {
+    const alphabet = Array.from({ length: 26 }, () => 0);
+    let i = 0;
+    let maxFreq = 0;
+    let maxLen = 0;
+    for (let j = 0; j < s.length; j++) {
+        const idx = s[j].charCodeAt(0) - 65;
+        alphabet[idx]++;
+        maxFreq = Math.max(maxFreq, alphabet[idx]);
+        while (j - i + 1 - maxFreq > k) {
+            alphabet[s[i].charCodeAt(0) - 65]--;
+            i++;
+        }
+        maxLen = Math.max(maxLen, j - i + 1);
+    }
+
+    return maxLen;
+};
