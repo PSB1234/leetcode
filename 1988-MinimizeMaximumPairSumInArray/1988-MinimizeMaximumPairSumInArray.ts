d923ba170e757748ab76bffa3e436835bc481f5c
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:17:06 +0530

    Create 1988-MinimizeMaximumPairSumInArray

---
+++ 1988-MinimizeMaximumPairSumInArray/1988-MinimizeMaximumPairSumInArray.ts
@@ -0,0 +1,14 @@
+// Last updated: 16/7/2026, 9:17:07 pm
+function minPairSum(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+    let left = 0;
+    let right = nums.length - 1;
+    let pair = nums.length / 2;
+    let max = -Infinity
+    while (left <= right) {
+        max = Math.max(max, nums[left] + nums[right]);
+        left++;
+        right--;
+    }
+    return max
+};
