48689ec651d1f9ca135a1f5e0233331f314b6576
Prathamesh <42248309+PSB1234@users.noreply.github.com>
2026-07-16 14:18:13 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.ts
@@ -0,0 +1,24 @@
+// Last updated: 16/7/2026, 9:18:14 pm
+function checkBananaSpeed(piles: number[], speed: number): number {
+    let k = 0;
+    for (let i = 0; i < piles.length; i++) {
+        k += Math.ceil(piles[i] / speed);
+    }
+    return k;
+}
+function minEatingSpeed(piles: number[], h: number): number {
+    let maxSpeed = Math.max(...piles);
+    let minSpeed = 1;
+    let middle = 0;
+    while (minSpeed <= maxSpeed) {
+        middle = minSpeed + Math.floor((maxSpeed - minSpeed) / 2);
+        const time = checkBananaSpeed(piles, middle);
+        if (time <= h) {
+            maxSpeed = middle - 1;
+        } else if (time > h) {
+            minSpeed = middle + 1;
+        }
+
+    }
+    return minSpeed;
+};
